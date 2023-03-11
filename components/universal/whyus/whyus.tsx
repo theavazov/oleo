a3f2ee3e629fdd4c4d0ca980896dd5b744894832
Doston04 <avazovjr@gmail.com>
2023-03-11 16:13:41 +0500

    whyus

--- components/universal/whyus/whyus.tsx
+++ components/universal/whyus/whyus.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ModalContext } from "../../../contexts/modal";
+import { TranslationsContext } from "../../../contexts/translations";
 import { buy } from "../../../public/icons";
 import { Button } from "../../utils/buttons/buttons";
 import styles from "./whyus.module.css";
@@ -25,29 +26,31 @@ export function WhyUsSection({ variant, className, title }: Props) {
 }
 
 const Stats = () => {
+  const { t } = useContext(TranslationsContext);
+
   return (
     <div className={styles.container}>
       <div className={styles.stats_div}>
-        <p>biz bilan oson bog’laning va hamkorlikni boshlang</p>
-        <h5>500+</h5>
+        <p>{t["whyus.about_desc1"]}</p>
+        <h5>{t["whyus.about_title1"]}</h5>
       </div>
       <div className={styles.stats_white}>
-        <p>Yillik ishlab chiqarish soni</p>
+        <p>{t["whyus.about_desc2"]}</p>
         <div className={styles.animation_box}>
           <svg className={styles.svg} viewBox="0 0 100 100">
             <circle className={styles.meter} cx="50" cy="50" r="40" />
           </svg>
-          <h6>500 T</h6>
+          <h6>{t["whyus.about_title2"]}</h6>
         </div>
       </div>
       <div className={styles.stats_right}>
         <div className={styles.stats_div}>
-          <p>Katta buyurtmalarni ham yetkazib bera olami oz vaqtida</p>
-          <h5 style={{ paddingLeft: "80px" }}>3K+</h5>
+          <p>{t["whyus.about_desc3"]}</p>
+          <h5 className={styles.yetim_title}>{t["whyus.about_title3"]}</h5>
         </div>
         <div className={styles.stats_div}>
-          <p>Katta buyurtmalarni ham yetkazib bera olami oz vaqtida</p>
-          <h5>50+</h5>
+          <p>{t["whyus.about_desc4"]}</p>
+          <h5>{t["whyus.about_title4"]}</h5>
         </div>
       </div>
     </div>
@@ -56,6 +59,7 @@ const Stats = () => {
 
 const Business = () => {
   const { setVariant, setIsModal } = useContext(ModalContext);
+  const { t } = useContext(TranslationsContext);
 
   const myFunction = () => {
     setVariant("business");
@@ -66,32 +70,27 @@ const Business = () => {
     <div className={styles.business_container}>
       <div className={styles.stats_container}>
         <div className={styles.stats_div}>
-          <p>biz bilan oson bog’laning va hamkorlikni boshlang</p>
-          <h5>B2B</h5>
+          <p>{t["whyus.business_desc1"]}</p>
+          <h5>{t["whyus.business_title1"]}</h5>
         </div>
         <div className={styles.stats_div}>
-          <p>Katta buyurtmalarni ham yetkazib bera olami oz vaqtida</p>
-          <h5 style={{ paddingLeft: "40px" }}>1 T</h5>
+          <p>{t["whyus.business_desc2"]}</p>
+          <h5 className={styles.yetim_title}>{t["whyus.business_title2"]}</h5>
         </div>
         <div className={styles.stats_white}>
-          <p>Qisqa vaqt ichida yetkazib beramiz. Kutib qolmaysiz</p>
+          <p>{t["whyus.business_desc3"]}</p>
           <div className={styles.animation_box}>
             <svg className={styles.svg} viewBox="0 0 100 100">
               <circle className={styles.meter} cx="50" cy="50" r="40" />
             </svg>
-            <h6>1 ish kuni</h6>
+            <h6>{t["whyus.business_title2"]}</h6>
           </div>
         </div>
       </div>
       <div className={styles.business_content}>
-        <p>
-          orem Ipsum has been the industry&apos;s standard dummy text ever since
-          the 1500s, when an unknown printer took a galley of type and scrambled
-          it to make a type specimen book. It has survived not only five1500s,
-          when an unknown printer took a galley of
-        </p>
+        <p>{t["whyus.business_last_text"]}</p>
         <Button variant="primary" icon={buy} customFunction={myFunction}>
-          Zayavka yuborish
+          {t["main.send_request"]}
         </Button>
       </div>
     </div>
